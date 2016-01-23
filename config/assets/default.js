75c321e552baef134087939e84d9cb785f8f8314
David Caballero <codelero@gmail.com>
2016-01-23 15:49:57 +0000

    fix(Product Code mask): replaced ui-tils with just ui-mask

--- config/assets/default.js
+++ config/assets/default.js
@@ -12,7 +12,7 @@ module.exports = {
         'public/lib/angular-resource/angular-resource.js',
         'public/lib/angular-animate/angular-animate.js',
         'public/lib/angular-ui-router/release/angular-ui-router.js',
-        'public/lib/angular-ui-utils/ui-utils.js',
+        'public/lib/angular-ui-mask/src/mask.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/AngularJS-Toaster/toaster.js',
         'public/lib/angular-file-upload/angular-file-upload.js'
