28bf6d795bb5b072a8afcdd8e3a8300502779e2a
David Caballero <codelero@gmail.com>
2016-01-26 08:53:55 +0000

    add charts resolve error handler

--- modules/charts/client/config/charts.client.routes.js
+++ modules/charts/client/config/charts.client.routes.js
@@ -4,16 +4,25 @@ angular.module('charts').config(['$stateProvider', function ($stateProvider) {
   $stateProvider
     .state('charts', {
       url: '/charts',
-      templateUrl:
-        'modules/charts/views/charts.client.view.html',
+      templateUrl: 'modules/charts/views/charts.client.view.html',
 
       controller: 'ChartsController',
       resolve: {
         productItems: function (Products) {
-          return Products.query().$promise;
+          return Products.query(function (response) {
+            // no code needed for success
+          }, function (response) {
+
+            if (response.status === 404) {
+              // alert error message with method and url
+              alert('Error accessing resource: ' + response.config.method + ' ' + response.config.url);
+            } else {
+              // alert status
+              alert(response.statusText);
+            }
+          }).$promise;
         }
       }
-      //templateUrl: 'modules/core/views/home.client.view.html'
     }
   );
 }]);
