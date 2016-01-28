468049874482533989425fbb8e3196916e133caf
David Caballero <codelero@gmail.com>
2016-01-28 22:23:36 +0000

    add mongolab production details

--- config/env/production.js
+++ config/env/production.js
@@ -4,10 +4,10 @@ module.exports = {
   secure: true,
   port: process.env.PORT || 8443,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'ds051665.mongolab.com:51665/business-app',
     options: {
-      user: '',
-      pass: ''
+      user: 'admin',
+      pass: 'bek;dYagg$ob'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
