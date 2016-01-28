d4ef64350dc9e9e39215a481304f77c3ee23d069
David Caballero <codelero@gmail.com>
2016-01-28 22:47:14 +0000

    add angularjs-toaster to production assets

--- config/assets/production.js
+++ config/assets/production.js
@@ -12,6 +12,7 @@ module.exports = {
         'public/lib/angular-resource/angular-resource.min.js',
         'public/lib/angular-animate/angular-animate.min.js',
         'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+        'public/lib/AngularJS-Toaster/toaster.js',
         'public/lib/angular-ui-mask/dist/mask.js',
         'public/lib/d3/d3.min.js',
         'public/lib/angular-charts/dist/angular-charts.min.js',        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
